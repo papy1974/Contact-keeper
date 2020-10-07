12f8bc76c9d409e77016a4b577b32b6ce6233752
Papy Lontomba <papylotomba@yahoo.fr>
2020-10-07 07:03:30 -0700

    coded contact model and contacts routes

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
+const { check, validationResult } = require("express-validator");
+
+const User = require("../models/User");
+const contact = require("../models/Contact");
 
 //@routes GET api/contacts
 // desc Get all users contacts
 //@access private
 
-router.get("/", (req, res) => {
-  res.send("Get all contacts");
+router.get("/", auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+    res.json(contacts);
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 //@routes POST api/contacts
