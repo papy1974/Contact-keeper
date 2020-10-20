4a81ae57e9f3f6d95e7f373760fc39a4acc3b750
Papy Lontomba <papylotomba@yahoo.fr>
2020-10-20 07:55:45 -0700

    coded contact and global State

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,71 @@
+import React, { useReduicer } from 'react';
+import uuid from 'uuid';
+import { SET_CURRENT } from '../types';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+    ADD_CONTACT,
+    DELETE_CONTACT,
+    SET_CURRENT,
+    CLEAR_CURRENT,
+    UPDATE_CONTACT,
+    FILTER_CONTACTS,
+    CLEAR_FILTER
+
+} from '../types';
+
+const ContactState = props => {
+    const initialState = {
+        contact: [
+            {
+                id:1,
+                name:'Jill Johson',
+                email:'jill@gmail.com',
+                phone:'111-111-1111',
+                type:'personal'
+            },
+
+            {
+                id:2,
+                name:'Bob Micheal',
+                email:'bob@gmail.com',
+                phone:'111-111-1112',
+                type:'personal'
+            },
+
+            {
+                id:3,
+                name:'Kelly  Johson',
+                email:'kelly@gmail.com',
+                phone:'111-111-1113',
+                type:'personal'
+            }
+
+            
+        ] 
+    };
+
+    const [state, dispatch] = useReduicer(contactReducer, initialState);
+
+    // ADD CONTACT
+    // DELETE CONTACT
+    // SET CURRENT CONTACT 
+    // CLEAR CURRENT CONTACT
+    //Update contact
+    // filter contacts
+    //clear filter
+
+    return (
+        <ContactContext.Provide
+        value= {{
+            contacts:state.contacts
+            
+        }}
+        >
+         {props.children}
+
+         </ContactContext.Provide>
+    );
+};
+
+export default ContactState
